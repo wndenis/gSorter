346fa9703b7bc4a7f90c5e443d2381c3e09c9b22
wndenis <winda.wzse@gmail.com>
2019-03-23 03:22:34 +0300

    Add webcam support

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pykinect2 import PyKinectV2
 from pykinect2.PyKinectV2 import *
 from pykinect2 import PyKinectRuntime
+import numpy as np
+import cv2
 
 if __name__ == "__main__":
+
+
     print("Start.")

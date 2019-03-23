72ac29f5986cce5b944dd25a4e56c13baa64f784
wndenis <winda.wzse@gmail.com>
2019-03-23 10:46:22 +0300

    ML and kinect image format workaround

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 # -*- coding: utf-8 -*-
 from pykinect2 import PyKinectV2
-from pykinect2.PyKinectV2 import *
 from pykinect2 import PyKinectRuntime
 import numpy as np
 import cv2
-import ctypes
+
+from imageai.Detection import ObjectDetection
+import os
+from time import time
+
+
+
 
 if __name__ == "__main__":
     print("Start.")
-    kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Infrared)
 
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+    detector.loadModel()
+
+    kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
+
+    shape = (424, 512)
+    midpoint = (kinect.color_frame_desc.Width // 2, kinect.color_frame_desc.Height // 2)
     while True:
-        frame = kinect.get_last_infrared_frame()
-        #frame = frame.astype(np.uint8)
-        frame = np.reshape(frame, (424, 512))
-        f8 = np.uint8(frame.clip(1, 4000) / 16.)
-        frame = np.dstack((f8, f8, f8))
-        #frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        frame = kinect.get_last_color_frame()
+
+        frame = np.reshape(frame, (1080, 1920, -1)).astype(np.uint8)
+        frame = cv2.resize(frame, dsize=(1920//4, 1080//4), interpolation=cv2.INTER_CUBIC)
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        detections = detector.detectObjectsFromImage(input_image=gray_frame,
+                                                     input_type="array",
+                                                     output_type="array",
+                                                     minimum_percentage_probability=10)
+        for box in detections[1]:
+            print(box)
+        print("\n\n\n\n\n\n\n")
+        # frame = np.reshape(frame, shape)
+        # frame = np.uint8(frame.clip(1, 4000) / 16.)?????????????????
         # frame = cv2.bilateralFilter(frame, 9, 150, 75)
-
-        # continue
-
-        # frame = kinect.get_last_infrared_frame()
-        # f8 = np.uint8(frame.clip(1, 4000) / 16.)
-        # frame = np.dstack((f8, f8, f8))
-        # # frame = np.array(frame)
-        # frame = frame.reshape(kinect.color_frame_desc.Width // 3, kinect.color_frame_desc.Height // 3)
-
-        print(frame)
-        print()
+        # cv2.rectangle(frame, (midpoint[0]-15, midpoint[1]-15), (midpoint[0]+15, midpoint[1]+15), (32000, 32000, 32000), 2)
+        # color = frame[midpoint]
+        #frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
 
         cv2.imshow('frame', frame)
 

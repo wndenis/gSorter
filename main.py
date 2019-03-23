c1de523f7da21ddac719db82b6e9b7e7dd1172dc
wndenis <winda.wzse@gmail.com>
2019-03-23 22:24:56 +0300

    It now works with GPU + custom boxes

--- main.py
+++ main.py
@@ -13,33 +13,41 @@ if __name__ == "__main__":
     execution_path = os.getcwd()
 
     detector = ObjectDetection()
-    # detector.setModelTypeAsYOLOv3()
-    # detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
-    detector.setModelTypeAsTinyYOLOv3()
-    detector.setModelPath(os.path.join(execution_path, "yolo-tiny.h5"))
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+    # detector.setModelTypeAsTinyYOLOv3()
+    # detector.setModelPath(os.path.join(execution_path, "yolo-tiny.h5"))
     detector.loadModel()
+    custom_objects = detector.CustomObjects(person=True, umbrella=True, car=True, cup=True, fork=True, knife=True)
 
     kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
 
     shape = (424, 512)
     midpoint = (kinect.color_frame_desc.Width // 2, kinect.color_frame_desc.Height // 2)
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
     while True:
         frame = kinect.get_last_color_frame()
+        frame = frame.reshape((1080, 1920, -1)).astype(np.uint8)
+        # frame = np.reshape(frame, (1080, 1920, -1))#.astype(np.uint8)
+        frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
 
-        frame = np.reshape(frame, (1080, 1920, -1)).astype(np.uint8)
-        frame = cv2.resize(frame, dsize=(1920//6, 1080//6), interpolation=cv2.INTER_CUBIC)
+        # frame = cv2.resize(frame, dsize=(1920//4, 1080//4), interpolation=cv2.INTER_CUBIC)
 
         #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if False:
+        if True:
             detections = detector.detectObjectsFromImage(input_image=frame,
                                                          input_type="array",
                                                          output_type="array",
-                                                         minimum_percentage_probability=10)
+                                                         minimum_percentage_probability=70)
             for box in detections[1]:
-                cv2.rectangle(frame, box['box_points'],
+                b = box['box_points']
+                cv2.rectangle(frame, b,
                               (0, 0, 250), 2)
-                print(box)
-                print("\n\n\n\n\n\n\n")
+                label = "{} {:.2f}".format(box['name'], box['percentage_probability'])
+                cv2.putText(frame, label, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+
+                # print(box)
+                # print("\n\n\n\n\n\n\n")
         # frame = np.reshape(frame, shape)
         # frame = np.uint8(frame.clip(1, 4000) / 16.)?????????????????
         # frame = cv2.bilateralFilter(frame, 9, 150, 75)
@@ -47,7 +55,7 @@ if __name__ == "__main__":
         # color = frame[midpoint]
         #frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
 
-        cv2.imshow('frame', frame)
+        cv2.imshow("output", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
